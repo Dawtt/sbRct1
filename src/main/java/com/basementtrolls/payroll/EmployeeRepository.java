a6e42b2d017b93c217d82e3a4dd817adff264484
dw <davidwhitebird@gmail.com>
2018-06-22 04:46:26 -0500

    Step I part II

--- src/main/java/com/basementtrolls/payroll/EmployeeRepository.java
+++ src/main/java/com/basementtrolls/payroll/EmployeeRepository.java
@@ -1,13 +1,15 @@
 package com.basementtrolls.payroll;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 
 /*
-The repository extends Spring Data Commons' CrudRepository and plugs in the type of the domain object and its primary key
+v.1 this repository extends Spring Data Commons' CrudRepository and plugs in the type of the domain object and its primary key
+v.2 To get underway with using frontend hypermedia controls, you need to turn on some extra controls. Spring Data REST provides paging support.
+
 */
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 }
