d8ecc6464c7cb1e0d0e49baa08b5f3f48f7ae7d6
dw <davidwhitebird@gmail.com>
2018-07-06 00:18:20 -0500

    This actually runs a server (with npm start)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+
+
+
+
+/*
+
 import React, { Component } from 'react';
+import ReactDOM from "react";
 
 
 
@@ -16,4 +32,4 @@ import React, { Component } from 'react';
     }
 }
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+ReactDOM.render(<Example />, document.getElementById('root'));*/
